c5f07ef6c673d1dae4f5bcf71cd06843ff9f3172
zuzusan <zuzusan@blue-stone.net>
2025-03-21 15:49:05 +0630

    [MOD] ion-header and [ADD] db.json

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  selectedSegment: string = 'all';
+
+  auditions = [
+    { title: 'Summer Season Audition', status: 'Available', color: 'success', image: '../../assets/image1.avif' },
+    { title: 'Winter Season Audition', status: 'Ongoing', color: 'warning', image: '../../assets/image4.avif' },
+    { title: 'Past Audition', status: 'Finished', color: 'tertiary', image: '../../assets/image3.avif' },
+    { title: 'Past Audition', status: 'Finished', color: 'tertiary', image: '../../assets/image5.avif' },
+    { title: 'Past Audition', status: 'Finished', color: 'tertiary', image: '../../assets/image2.avif' }
+  ];
+
   constructor() {}
+
+  getFilteredAuditions(){
+    if(this.selectedSegment === 'all'){
+      return this.auditions;
+    }
+    return this.auditions.filter(audition => audition.status === this.selectedSegment);
+  }
 
 }
